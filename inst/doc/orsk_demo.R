cfd0ea4df927779aa570f2ec85a8ac51334ab5b1
Zhu Wang <wangz1@uthscsa.edu>
2019-01-12 22:02:53 +0000

    version 1.0-5

--- inst/doc/orsk_demo.R
+++ inst/doc/orsk_demo.R
@@ -1,38 +1,38 @@
 ### R code from vignette source 'orsk_demo.Rnw'
 
 ###################################################
-### code chunk number 1: orsk_demo.Rnw:157-158
+### code chunk number 1: orsk_demo.Rnw:154-155
 ###################################################
 options(prompt = "R> ", continue = "+  ", width = 70, useFancyQuotes = FALSE)
 
 
 ###################################################
-### code chunk number 2: orsk_demo.Rnw:160-161 (eval = FALSE)
+### code chunk number 2: orsk_demo.Rnw:157-158 (eval = FALSE)
 ###################################################
 ## install.packages("orsk")
 
 
 ###################################################
-### code chunk number 3: orsk_demo.Rnw:164-166 (eval = FALSE)
+### code chunk number 3: orsk_demo.Rnw:161-163 (eval = FALSE)
 ###################################################
 ## library("orsk")
 ## vignette("orsk_demo",package = "orsk")
 
 
 ###################################################
-### code chunk number 4: orsk_demo.Rnw:169-170 (eval = FALSE)
+### code chunk number 4: orsk_demo.Rnw:166-167 (eval = FALSE)
 ###################################################
 ## edit(vignette("orsk_demo",package = "orsk"))
 
 
 ###################################################
-### code chunk number 5: orsk_demo.Rnw:180-181
+### code chunk number 5: orsk_demo.Rnw:177-178
 ###################################################
 library("orsk")
 
 
 ###################################################
-### code chunk number 6: orsk_demo.Rnw:183-186
+### code chunk number 6: orsk_demo.Rnw:180-183
 ###################################################
 ### analysis of Table 2 with grid method
 res1 <- orsk(nctr=1636, ntrt=2601, a=2.61, al=2.25, au= 3.03, method="grid")
@@ -40,19 +40,19 @@ summary(res1)
 
 
 ###################################################
-### code chunk number 7: orsk_demo.Rnw:193-194
+### code chunk number 7: orsk_demo.Rnw:190-191
 ###################################################
 plot(res1, type="OR")
 
 
 ###################################################
-### code chunk number 8: orsk_demo.Rnw:202-203
+### code chunk number 8: orsk_demo.Rnw:199-200
 ###################################################
 plot(res1, type="RR")
 
 
 ###################################################
-### code chunk number 9: orsk_demo.Rnw:222-226
+### code chunk number 9: orsk_demo.Rnw:219-223
 ###################################################
 ### analysis of Table 2 with optim method
 require("setRNG")
@@ -61,26 +61,26 @@ seed=579))
 
 
 ###################################################
-### code chunk number 10: orsk_demo.Rnw:228-229 (eval = FALSE)
+### code chunk number 10: orsk_demo.Rnw:225-226 (eval = FALSE)
 ###################################################
 ## res2 <- orsk(nctr=1636, ntrt=2601, a=2.61, al=2.25, au= 3.03, method="optim")
 
 
 ###################################################
-### code chunk number 11: orsk_demo.Rnw:231-232
+### code chunk number 11: orsk_demo.Rnw:228-229
 ###################################################
 res2 <- orsk(nctr=1636, ntrt=2601, a=2.61, al=2.25, au= 3.03, method="optim")
 
 
 ###################################################
-### code chunk number 12: orsk_demo.Rnw:234-236
+### code chunk number 12: orsk_demo.Rnw:231-233
 ###################################################
 summary(res2)
 summary(res2$res$RR)
 
 
 ###################################################
-### code chunk number 13: orsk_demo.Rnw:238-241
+### code chunk number 13: orsk_demo.Rnw:235-238
 ###################################################
 #compare the computing speed between the two methods of estimation. Time to finish the modeling
 #time.optim <- system.time(orsk(nctr=1636, ntrt=2601, a=2.61, al=2.25, au=3.03, method="optim"))[1]
